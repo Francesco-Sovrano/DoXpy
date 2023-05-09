8487b3e73cca994c466f0e59355122991cfc0e10
francesco.sovrano <38495561+Francesco-Sovrano@users.noreply.github.com>
2023-05-09 17:27:55 +0200

    Added fix for windows
    Added fix for windows

--- demo/assess_degree_of_explainability.py
+++ demo/assess_degree_of_explainability.py
@@ -145,108 +145,109 @@ else:
 		'with_cache': True,
 	}
 
-################ Initialise data structures ################
-explicandum_graph_cache = os.path.join(cache_path,f"cache_explicandum_graph_lemma-{GRAPH_BUILDER_OPTIONS['lemmatize_label']}.pkl")
-explainable_information_graph_cache = os.path.join(cache_path,f"cache_explainable_information_graph_lemma-{GRAPH_BUILDER_OPTIONS['lemmatize_label']}.pkl")
-edu_graph_cache = os.path.join(cache_path,f"cache_edu_graph.pkl")
-betweenness_centrality_cache = os.path.join(cache_path,'cache_betweenness_centrality.pkl')
+if __name__=='__main__':
+	################ Initialise data structures ################
+	explicandum_graph_cache = os.path.join(cache_path,f"cache_explicandum_graph_lemma-{GRAPH_BUILDER_OPTIONS['lemmatize_label']}.pkl")
+	explainable_information_graph_cache = os.path.join(cache_path,f"cache_explainable_information_graph_lemma-{GRAPH_BUILDER_OPTIONS['lemmatize_label']}.pkl")
+	edu_graph_cache = os.path.join(cache_path,f"cache_edu_graph.pkl")
+	betweenness_centrality_cache = os.path.join(cache_path,'cache_betweenness_centrality.pkl')
 
-qa_cache = os.path.join(cache_path,'cache_qa_embedder.pkl')
-qa_edu_cache = os.path.join(cache_path,'cache_qa_edu_embedder.pkl')
-qa_disco_cache = os.path.join(cache_path,'cache_qa_disco_embedder.pkl')
-########################################################################
-print('Building Graph..')
-explicandum_graph = load_or_create_cache(
-	explicandum_graph_cache, 
-	lambda: KnowledgeGraphExtractor(GRAPH_BUILDER_OPTIONS).set_documents_path(explicandum_path, remove_stopwords=True, remove_numbers=True, avoid_jumps=True).build(**GRAPH_EXTRACTION_OPTIONS)
-)
-save_graphml(explicandum_graph, os.path.join(cache_path,'explicandum_graph'))
-print('Explicandum Graph size:', len(explicandum_graph))
-print("Explicandum Graph clauses:", len(list(filter(lambda x: '{obj}' in x[1], explicandum_graph))))
-explainable_information_graph = load_or_create_cache(
-	explainable_information_graph_cache, 
-	lambda: KnowledgeGraphExtractor(GRAPH_BUILDER_OPTIONS).set_documents_path(explainable_information_path, **GRAPH_CLEANING_OPTIONS).build(**GRAPH_EXTRACTION_OPTIONS)
-)
-save_graphml(explainable_information_graph, os.path.join(cache_path,'explainable_information_graph'))
-print('Explainable Information Graph size:', len(explainable_information_graph))
-print("Explainable Information Graph clauses:", len(list(filter(lambda x: '{obj}' in x[1], explainable_information_graph))))
-#############
-print('Building Question Answerer..')
-# betweenness_centrality = load_or_create_cache(
-# 	betweenness_centrality_cache, 
-# 	lambda: get_betweenness_centrality(filter(lambda x: '{obj}' in x[1], explainable_information_graph))
-# )
-kg_manager = KnowledgeGraphManager(KG_MANAGER_OPTIONS, explainable_information_graph)
-qa = AnswerRetriever( # Using qa_dict_list also for getting the archetype_fitness_dict might over-estimate the median pertinence of some archetypes (and in a different way for each), because the QA Extractor is set to prefer a higher recall to a higher precision.
-	kg_manager= kg_manager, 
-	concept_classifier_options= CONCEPT_CLASSIFIER_OPTIONS, 
-	sentence_classifier_options= SENTENCE_CLASSIFIER_OPTIONS, 
-	# betweenness_centrality= betweenness_centrality,
-)
-qa.load_cache(qa_cache)
+	qa_cache = os.path.join(cache_path,'cache_qa_embedder.pkl')
+	qa_edu_cache = os.path.join(cache_path,'cache_qa_edu_embedder.pkl')
+	qa_disco_cache = os.path.join(cache_path,'cache_qa_disco_embedder.pkl')
+	########################################################################
+	print('Building Graph..')
+	explicandum_graph = load_or_create_cache(
+		explicandum_graph_cache, 
+		lambda: KnowledgeGraphExtractor(GRAPH_BUILDER_OPTIONS).set_documents_path(explicandum_path, remove_stopwords=True, remove_numbers=True, avoid_jumps=True).build(**GRAPH_EXTRACTION_OPTIONS)
+	)
+	save_graphml(explicandum_graph, os.path.join(cache_path,'explicandum_graph'))
+	print('Explicandum Graph size:', len(explicandum_graph))
+	print("Explicandum Graph clauses:", len(list(filter(lambda x: '{obj}' in x[1], explicandum_graph))))
+	explainable_information_graph = load_or_create_cache(
+		explainable_information_graph_cache, 
+		lambda: KnowledgeGraphExtractor(GRAPH_BUILDER_OPTIONS).set_documents_path(explainable_information_path, **GRAPH_CLEANING_OPTIONS).build(**GRAPH_EXTRACTION_OPTIONS)
+	)
+	save_graphml(explainable_information_graph, os.path.join(cache_path,'explainable_information_graph'))
+	print('Explainable Information Graph size:', len(explainable_information_graph))
+	print("Explainable Information Graph clauses:", len(list(filter(lambda x: '{obj}' in x[1], explainable_information_graph))))
+	#############
+	print('Building Question Answerer..')
+	# betweenness_centrality = load_or_create_cache(
+	# 	betweenness_centrality_cache, 
+	# 	lambda: get_betweenness_centrality(filter(lambda x: '{obj}' in x[1], explainable_information_graph))
+	# )
+	kg_manager = KnowledgeGraphManager(KG_MANAGER_OPTIONS, explainable_information_graph)
+	qa = AnswerRetriever( # Using qa_dict_list also for getting the archetype_fitness_dict might over-estimate the median pertinence of some archetypes (and in a different way for each), because the QA Extractor is set to prefer a higher recall to a higher precision.
+		kg_manager= kg_manager, 
+		concept_classifier_options= CONCEPT_CLASSIFIER_OPTIONS, 
+		sentence_classifier_options= SENTENCE_CLASSIFIER_OPTIONS, 
+		# betweenness_centrality= betweenness_centrality,
+	)
+	qa.load_cache(qa_cache)
 
-########################################################################################################################################################################
-########################################################################################################################################################################
-########################################################################################################################################################################
+	########################################################################################################################################################################
+	########################################################################################################################################################################
+	########################################################################################################################################################################
 
-### Get explanandum aspects
-explanandum_aspect_list = get_concept_description_dict(graph=explicandum_graph, label_predicate=HAS_LABEL_PREDICATE, valid_concept_filter_fn=lambda x: '{obj}' in x[1]).keys()
-explanandum_aspect_list = list(explanandum_aspect_list)
-print('Important explicandum aspects:', len(explanandum_aspect_list))
-print(json.dumps(explanandum_aspect_list, indent=4))
+	### Get explanandum aspects
+	explanandum_aspect_list = get_concept_description_dict(graph=explicandum_graph, label_predicate=HAS_LABEL_PREDICATE, valid_concept_filter_fn=lambda x: '{obj}' in x[1]).keys()
+	explanandum_aspect_list = list(explanandum_aspect_list)
+	print('Important explicandum aspects:', len(explanandum_aspect_list))
+	print(json.dumps(explanandum_aspect_list, indent=4))
 
-### Define archetypal questions
-question_template_list = [
-	##### AMR
-	'What is {X}?',
-	'Who is {X}?',
-	'How is {X}?',
-	'Where is {X}?',
-	'When is {X}?',
-	'Which {X}?',
-	'Whose {X}?',
-	'Why {X}?',
-	##### Discourse Relations
-	'In what manner is {X}?', # (25\%),
-	'What is the reason for {X}?', # (19\%),
-	'What is the result of {X}?', # (16\%),
-	'What is an example of {X}?', # (11\%),
-	'After what is {X}?', # (7\%),
-	'While what is {X}?', # (6\%),
-	'In what case is {X}?', # (3),
-	'Despite what is {X}?', # (3\%),
-	'What is contrasted with {X}?', # (2\%),
-	'Before what is {X}?', # (2\%),
-	'Since when is {X}?', # (2\%),
-	'What is similar to {X}?', # (1\%),
-	'Until when is {X}?', # (1\%),
-	'Instead of what is {X}?', # (1\%),
-	'What is an alternative to {X}?', # ($\leq 1\%$),
-	'Except when it is {X}?', # ($\leq 1\%$),
-	'{X}, unless what?', # ($\leq 1\%$).
-]
+	### Define archetypal questions
+	question_template_list = [
+		##### AMR
+		'What is {X}?',
+		'Who is {X}?',
+		'How is {X}?',
+		'Where is {X}?',
+		'When is {X}?',
+		'Which {X}?',
+		'Whose {X}?',
+		'Why {X}?',
+		##### Discourse Relations
+		'In what manner is {X}?', # (25\%),
+		'What is the reason for {X}?', # (19\%),
+		'What is the result of {X}?', # (16\%),
+		'What is an example of {X}?', # (11\%),
+		'After what is {X}?', # (7\%),
+		'While what is {X}?', # (6\%),
+		'In what case is {X}?', # (3),
+		'Despite what is {X}?', # (3\%),
+		'What is contrasted with {X}?', # (2\%),
+		'Before what is {X}?', # (2\%),
+		'Since when is {X}?', # (2\%),
+		'What is similar to {X}?', # (1\%),
+		'Until when is {X}?', # (1\%),
+		'Instead of what is {X}?', # (1\%),
+		'What is an alternative to {X}?', # ($\leq 1\%$),
+		'Except when it is {X}?', # ($\leq 1\%$),
+		'{X}, unless what?', # ($\leq 1\%$).
+	]
 
-### Define a question generator
-question_generator = lambda question_template,concept_label: question_template.replace('{X}',concept_label)
+	### Define a question generator
+	question_generator = lambda question_template,concept_label: question_template.replace('{X}',concept_label)
 
-### Initialise the DoX estimator
-dox_estimator = DoXEstimator(qa)
-### Estimate DoX
-dox = dox_estimator.estimate(
-	aspect_uri_iter=list(explanandum_aspect_list), 
-	query_template_list=question_template_list, 
-	question_generator=question_generator,
-	**ARCHETYPE_FITNESS_OPTIONS, 
-)
-print(f'DoX:', json.dumps(dox, indent=4))
-### Compute the average DoX
-# archetype_weight_dict = {
-# 	'why': 1,
-# 	'how': 0.9,
-# 	'what-for': 0.75,
-# 	'what': 0.75,
-# 	'what-if': 0.6,
-# 	'when': 0.5,
-# }
-average_dox = dox_estimator.get_weighted_degree_of_explainability(dox, archetype_weight_dict=None)
-print('Average DoX:', average_dox)
+	### Initialise the DoX estimator
+	dox_estimator = DoXEstimator(qa)
+	### Estimate DoX
+	dox = dox_estimator.estimate(
+		aspect_uri_iter=list(explanandum_aspect_list), 
+		query_template_list=question_template_list, 
+		question_generator=question_generator,
+		**ARCHETYPE_FITNESS_OPTIONS, 
+	)
+	print(f'DoX:', json.dumps(dox, indent=4))
+	### Compute the average DoX
+	# archetype_weight_dict = {
+	# 	'why': 1,
+	# 	'how': 0.9,
+	# 	'what-for': 0.75,
+	# 	'what': 0.75,
+	# 	'what-if': 0.6,
+	# 	'when': 0.5,
+	# }
+	average_dox = dox_estimator.get_weighted_degree_of_explainability(dox, archetype_weight_dict=None)
+	print('Average DoX:', average_dox)
--- simple_example.py
+++ simple_example.py
@@ -123,73 +123,74 @@ SENTENCE_CLASSIFIER_DEFAULT_OPTIONS = {
 	'default_tfidf_importance': 0,
 }
 
-################ Initialise data structures ################
-print('Building Graph..')
-explainable_information_graph = KnowledgeGraphExtractor(KG_BUILDER_DEFAULT_OPTIONS).set_content_list(PHI, remove_stopwords=False, remove_numbers=False, avoid_jumps=True).build()
-# save_graphml(explainable_information_graph, 'knowledge_graph')
-print('Graph size:', len(explainable_information_graph))
-print("Graph's Clauses:", len(list(filter(lambda x: '{obj}' in x[1], explainable_information_graph))))
-#############
-print('Building Question Answerer..')
-# betweenness_centrality = get_betweenness_centrality(filter(lambda x: '{obj}' in x[1], explainable_information_graph))
-kg_manager = KnowledgeGraphManager(KG_MANAGER_OPTIONS, explainable_information_graph)
-qa = AnswerRetriever( # Using qa_dict_list also for getting the archetype_fitness_dict might over-estimate the median pertinence of some archetypes (and in a different way for each), because the QA Extractor is set to prefer a higher recall to a higher precision.
-	kg_manager= kg_manager, 
-	concept_classifier_options= CONCEPT_CLASSIFIER_DEFAULT_OPTIONS, 
-	sentence_classifier_options= SENTENCE_CLASSIFIER_DEFAULT_OPTIONS, 
-	# betweenness_centrality= betweenness_centrality,
-)
-########################################################
+if __name__=='__main__':
+	################ Initialise data structures ################
+	print('Building Graph..')
+	explainable_information_graph = KnowledgeGraphExtractor(KG_BUILDER_DEFAULT_OPTIONS).set_content_list(PHI, remove_stopwords=False, remove_numbers=False, avoid_jumps=True).build()
+	# save_graphml(explainable_information_graph, 'knowledge_graph')
+	print('Graph size:', len(explainable_information_graph))
+	print("Graph's Clauses:", len(list(filter(lambda x: '{obj}' in x[1], explainable_information_graph))))
+	#############
+	print('Building Question Answerer..')
+	# betweenness_centrality = get_betweenness_centrality(filter(lambda x: '{obj}' in x[1], explainable_information_graph))
+	kg_manager = KnowledgeGraphManager(KG_MANAGER_OPTIONS, explainable_information_graph)
+	qa = AnswerRetriever( # Using qa_dict_list also for getting the archetype_fitness_dict might over-estimate the median pertinence of some archetypes (and in a different way for each), because the QA Extractor is set to prefer a higher recall to a higher precision.
+		kg_manager= kg_manager, 
+		concept_classifier_options= CONCEPT_CLASSIFIER_DEFAULT_OPTIONS, 
+		sentence_classifier_options= SENTENCE_CLASSIFIER_DEFAULT_OPTIONS, 
+		# betweenness_centrality= betweenness_centrality,
+	)
+	########################################################
 
-### Get explanandum aspects
-explanandum_aspect_list = EXPLANANDUM_ASPECTS
-print('Important explicandum aspects:', len(explanandum_aspect_list))
-print(json.dumps(explanandum_aspect_list, indent=4))
+	### Get explanandum aspects
+	explanandum_aspect_list = EXPLANANDUM_ASPECTS
+	print('Important explicandum aspects:', len(explanandum_aspect_list))
+	print(json.dumps(explanandum_aspect_list, indent=4))
 
-### Define archetypal questions
-question_template_list = [ # Q: the archetypal questions
-	##### AMR
-	'What is {X}?',
-	'Who is {X}?',
-	'How is {X}?',
-	'Where is {X}?',
-	'When is {X}?',
-	'Which {X}?',
-	'Whose {X}?',
-	'Why {X}?',
-	##### Discourse Relations
-	'In what manner is {X}?', # (25\%),
-	'What is the reason for {X}?', # (19\%),
-	'What is the result of {X}?', # (16\%),
-	'What is an example of {X}?', # (11\%),
-	'After what is {X}?', # (7\%),
-	'While what is {X}?', # (6\%),
-	'In what case is {X}?', # (3),
-	'Despite what is {X}?', # (3\%),
-	'What is contrasted with {X}?', # (2\%),
-	'Before what is {X}?', # (2\%),
-	'Since when is {X}?', # (2\%),
-	'What is similar to {X}?', # (1\%),
-	'Until when is {X}?', # (1\%),
-	'Instead of what is {X}?', # (1\%),
-	'What is an alternative to {X}?', # ($\leq 1\%$),
-	'Except when it is {X}?', # ($\leq 1\%$),
-	'{X}, unless what?', # ($\leq 1\%$).
-]
+	### Define archetypal questions
+	question_template_list = [ # Q: the archetypal questions
+		##### AMR
+		'What is {X}?',
+		'Who is {X}?',
+		'How is {X}?',
+		'Where is {X}?',
+		'When is {X}?',
+		'Which {X}?',
+		'Whose {X}?',
+		'Why {X}?',
+		##### Discourse Relations
+		'In what manner is {X}?', # (25\%),
+		'What is the reason for {X}?', # (19\%),
+		'What is the result of {X}?', # (16\%),
+		'What is an example of {X}?', # (11\%),
+		'After what is {X}?', # (7\%),
+		'While what is {X}?', # (6\%),
+		'In what case is {X}?', # (3),
+		'Despite what is {X}?', # (3\%),
+		'What is contrasted with {X}?', # (2\%),
+		'Before what is {X}?', # (2\%),
+		'Since when is {X}?', # (2\%),
+		'What is similar to {X}?', # (1\%),
+		'Until when is {X}?', # (1\%),
+		'Instead of what is {X}?', # (1\%),
+		'What is an alternative to {X}?', # ($\leq 1\%$),
+		'Except when it is {X}?', # ($\leq 1\%$),
+		'{X}, unless what?', # ($\leq 1\%$).
+	]
 
-### Define a question generator
-question_generator = lambda question_template,concept_label: question_template.replace('{X}',concept_label)
+	### Define a question generator
+	question_generator = lambda question_template,concept_label: question_template.replace('{X}',concept_label)
 
-### Initialise the DoX estimator
-dox_estimator = DoXEstimator(qa)
-### Estimate DoX
-dox = dox_estimator.estimate(
-	aspect_uri_iter=list(explanandum_aspect_list), 
-	query_template_list=question_template_list, 
-	question_generator=question_generator,
-	**ARCHETYPE_FITNESS_OPTIONS, 
-)
-print(f'DoX:', json.dumps(dox, indent=4))
-### Compute the average DoX
-average_dox = dox_estimator.get_weighted_degree_of_explainability(dox, archetype_weight_dict=None)
-print('Average DoX:', average_dox)
+	### Initialise the DoX estimator
+	dox_estimator = DoXEstimator(qa)
+	### Estimate DoX
+	dox = dox_estimator.estimate(
+		aspect_uri_iter=list(explanandum_aspect_list), 
+		query_template_list=question_template_list, 
+		question_generator=question_generator,
+		**ARCHETYPE_FITNESS_OPTIONS, 
+	)
+	print(f'DoX:', json.dumps(dox, indent=4))
+	### Compute the average DoX
+	average_dox = dox_estimator.get_weighted_degree_of_explainability(dox, archetype_weight_dict=None)
+	print('Average DoX:', average_dox)
